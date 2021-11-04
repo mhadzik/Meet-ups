718b7f526e032b27c699eff9462523ca4afceeaa
mhadzik <mikolaj.hadzik@gmail.com>
2021-11-04 19:03:47 +0100

    Add Modal component, styling, move form to modal component

--- src/App.svelte
+++ src/App.svelte
@@ -63,6 +63,10 @@
 
     meetups = updatedMeetups;
   };
+
+  const cancelEdit = () => {
+    editMode = null;
+  };
 </script>
 
 <Header />
@@ -71,7 +75,7 @@
     <Button on:click={() => (editMode = "add")}>New Meetup</Button>
   </div>
   {#if editMode === "add"}
-    <EditMeetup on:save={addMeetup} />
+    <EditMeetup on:save={addMeetup} on:cancel={cancelEdit} />
   {/if}
   <MeetupGrid {meetups} on:togglefavorite={toggleFavorite} />
 </main>
--- src/Meetups/EditMeetup.svelte
+++ src/Meetups/EditMeetup.svelte
@@ -2,6 +2,7 @@
   import TextInput from "../UI/TextInput.svelte";
   import Button from "../UI/Button.svelte";
   import { createEventDispatcher } from "svelte";
+  import Modal from "../UI/Modal.svelte";
 
   let title = "";
   let subtitle = "";
@@ -22,55 +23,63 @@
       description: description,
     });
   };
+
+  const cancel = () => {
+    dispatch("cancel");
+  };
 </script>
 
-<form on:submit|preventDefault={submitForm}>
-  <TextInput
-    id="title"
-    label="Title"
-    value={title}
-    on:input={(event) => (title = event.target.value)}
-  />
-  <TextInput
-    id="subtitle"
-    label="Subtitle"
-    value={subtitle}
-    on:input={(event) => (subtitle = event.target.value)}
-  />
-  <TextInput
-    id="address"
-    label="Address"
-    value={address}
-    on:input={(event) => (address = event.target.value)}
-  />
-  <TextInput
-    id="imageUrl"
-    label="Image URL"
-    value={imageUrl}
-    on:input={(event) => (imageUrl = event.target.value)}
-  />
-  <TextInput
-    id="email"
-    label="Email"
-    value={email}
-    type="email"
-    on:input={(event) => (email = event.target.value)}
-  />
-  <TextInput
-    id="description"
-    label="Description"
-    value={description}
-    controlType="textarea"
-    rows="3"
-    on:input={(event) => (description = event.target.value)}
-  />
-  <Button type="submit">Save</Button>
-</form>
+<Modal title="Edit Meetup Data" on:cancel>
+  <form on:submit|preventDefault={submitForm}>
+    <TextInput
+      id="title"
+      label="Title"
+      value={title}
+      on:input={(event) => (title = event.target.value)}
+    />
+    <TextInput
+      id="subtitle"
+      label="Subtitle"
+      value={subtitle}
+      on:input={(event) => (subtitle = event.target.value)}
+    />
+    <TextInput
+      id="address"
+      label="Address"
+      value={address}
+      on:input={(event) => (address = event.target.value)}
+    />
+    <TextInput
+      id="imageUrl"
+      label="Image URL"
+      value={imageUrl}
+      on:input={(event) => (imageUrl = event.target.value)}
+    />
+    <TextInput
+      id="email"
+      label="Email"
+      value={email}
+      type="email"
+      on:input={(event) => (email = event.target.value)}
+    />
+    <TextInput
+      id="description"
+      label="Description"
+      value={description}
+      controlType="textarea"
+      rows="3"
+      on:input={(event) => (description = event.target.value)}
+    />
+  </form>
+  <div slot="footer">
+    <Button type="button" mode="outline" on:click={cancel}>Cancel</Button>
+
+    <Button type="button" on:click={submitForm}>Save</Button>
+  </div>
+</Modal>
 
 <style>
   form {
-    width: 30rem;
-    max-width: 90%;
-    margin: auto;
+    width: 100%;
   }
 </style>
